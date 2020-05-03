2d42baa1bfc7bb82b91c0d0a4689d0343f4fc3be
LukasTsunami <prateadow@gmail.com>
2020-05-03 12:26:23 -0300

    configuring lighthouse graphql

--- app/User.php
+++ app/User.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -19,21 +17,13 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    // ...
 
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
